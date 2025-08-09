f8d79c17a58df336aaf27c3693cef52783c232fe
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-09 15:51:07 +0300

    директивы условной компиляции

--- example.c
+++ example.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
-#define X_N(N) x ## N
-#define print(a,b,c) printf("%d", a##b##c);
+# define LANG_C
 
-int main(void) 
+# if defined(LANG_C)
+#   include <stdio.h>
+# else
+#   include <iostream>
+# endif
+
+
+int main(void)
 {
-    int x1 = 1, x2 = -2, x4 = 10;
-    printf("%d\n", X_N(4));
-    print(19, 10, 1994);
-
+    int x = 5;
+#ifdef LANG_C
+    printf("%d\n", x);
+#else
+    std::cout << x << std::endl;
+#endif
     return 0;
 }
