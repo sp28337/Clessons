d846958fd324ad7978c1f025a372119c12b4fb68
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-13 08:58:53 +0300

    array declaration

--- example.c
+++ example.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
+#define SIZE_BUFFER 1024
+
 int main(void)
 {
-    int g = 476789;
-    char *ptr = (char*)&g;
+    // correct array declaration
+    double f[30];
+    char buffer[SIZE_BUFFER];
+    int marks[13];
+    short arr[3 * 8];
+    char bytes[sizeof(double)];
 
-    for(int i = 0; i < sizeof(int); ++i)
-        printf("%d ", *ptr++);
+    // sizeof() - is not a function, it is an operator that returns the size of a type or object
+
+    // incorrect array declaration
+    int n = 5;
+
+    int arr[n]; // correct in c99, not recommended due to possible incompatibility with other compilers
+    float func[21.5];
+    int null_arr[0];
 
     return 0;
 }
