4327a64305a4b6774e662396238114b70e016b7a
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-13 09:24:31 +0300

    calculate size of array

--- example.c
+++ example.c
@@ -6,25 +6,15 @@ int main(void)
 {
     int marks[TOTAL_MARKS];
 
-    // writing to the array
-    marks[0] = 1001;
+    marks[0] = 2 * 3;
     marks[2] = 4;
-    marks[3] = 5 * 5;
-    marks[12] = 6 - 1;
+    marks[12] = 7 - 2;
 
-    // reading from the array
-    int x = marks[0];
-    int y = marks[12];
-    int b = marks[3];
-
-    printf("x = %d\n", x);
-    printf("y = %d\n", y);
-    printf("b = %d\n\n", b);
+    size_t bytes_marks = sizeof(marks);
+    printf("sizeof(marks) = %zu bytes\n\n", bytes_marks);
 
     for(int i = 0; i < TOTAL_MARKS; ++i)
-    {
-        printf("%d) %d\n", i, marks[i]);
-    }
+        printf("[%d] - %d\n", i, marks[i]);
 
     return 0;
 }
