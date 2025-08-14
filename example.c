33f938be8bd99f8aab4b10798572e33f6486c945
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-14 14:27:31 +0300

    copy data from array into another

--- example.c
+++ example.c
@@ -2,18 +2,23 @@
 
 int main(void)
 {
-    int ptr_array[10] = {1, 2, 3, 4, 5, 6, 7};
-    int val = *(ptr_array + 1);
-    printf("%d", val);
+    float array_1[20] = {0.1, -3.6, 12.8, -0.2, 0, 27.5};
+    float array_2[10];
 
-    short arr[] = {4, 3, 2, 1, 5, 6, 7};
+    int size_1 = sizeof(array_1) / sizeof(array_1[0]);
+    int size_2 = sizeof(array_2) / sizeof(array_2[0]);
+    int shorter_size = (size_1 > size_2) ? size_2 : size_1;
 
-    short *ptr_arr;
-
-    ptr_arr = arr;
-
-    size_t len_1 = sizeof(arr);
-    size_t len_2 = sizeof(ptr_arr);
+    for(int i = 0; i < shorter_size; ++i)
+        printf("%.2f ", array_2[i]);
     
+    printf("\n");
+
+    for(int i = 0; i < shorter_size; ++i)
+        array_2[i] = array_1[i];
+
+    for(int i = 0; i < shorter_size; ++i)
+        printf("%.2f ", array_2[i]);
+
     return 0;
 }
