c13ac5d3c7a9fffb05432327a64b73b5e8464226
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-17 08:52:06 +0300

    matrix declaration

--- example.c
+++ example.c
@@ -1,28 +1,14 @@
 #include <stdio.h>
 
-// selection sort
 int main(void)
 {
-    int arr[] = {108, -3, 5, 0, -8, 1, 10, -22, 3, 18, -6, 9, -8};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int min;
+    char game_pole[3][3] = {{1, 2}, {3, 4}};
 
-    for(int i = 0; i < size - 1; ++i) {
-        int min_idx = i;
-        for(int j = i + 1; j < size; ++j) {
-            // if need DESC sort: arr[min_idx] < arr[j]
-            if (arr[min_idx] > arr[j]) {
-                min_idx = j;
-            }
-        }
-        if (min_idx != i) {
-            int temp = arr[i];
-            arr[i] = arr[min_idx];
-            arr[min_idx] = temp;
-        }
+    for(int i = 0; i < 3; ++i) {
+        for(int j = 0; j < 3; ++j)
+            printf("%d ", game_pole[i][j]);
+        printf("\n");
     }
-    for(int i = 0; i < size; ++i)
-        printf("%d ", arr[i]);
 
     return 0;
 }
