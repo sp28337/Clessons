12265ec5c0740841f434133bf8e30bc8ce4b6e2b
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-22 09:22:52 +0300

    streeng comparsion (strcmp())

--- example.c
+++ example.c
@@ -6,23 +6,11 @@ int main(void)
     char st1[20] = "Hello Pattaya!";
     char st2[30] = "Hello Pattaya!";
 
-    const char *str1 = st1;
-    const char *str2 = st2;
-    int i = 0;
-
-    for(; str1[i] != '\0' && str2[i] != '\0'; ++i) {
-        if(str1[i] != str2[i]) {
-            puts("Streeng are not equal");
-            return 0;
-        }
+    if(strcmp(st1, st2) == 0) {
+        puts("Streengs are equal");
+    } else {
+        puts("Streengs are not equal");
     }
-
-    if(str1[i] != str2[i]) {
-        puts("Streeng lengh are not equal");
-        return 0;
-    }
-
-    puts("Streeng are equal");
 
     return 0;
 }
