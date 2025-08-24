a31c97551d92d91c2128186e709cece6f9165aea
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-24 11:50:57 +0300

    convert str to int (ato[i|l|ll|f]())

--- example.c
+++ example.c
@@ -5,12 +5,21 @@
 
 int main(void)
 {
-    int num = -123;
-    char string[10];
+    int a = atoi("-123");
+    long b = atol("123124125");
+    long long c = atoll("12421512312412515");
+    double d = atof("0.2136");
 
-    sprintf(string, "%d", num);
+    printf("\na = %d\nb = %ld\nc = %lld\nd = %.2f\n", a, b, c, d);
 
-    puts(string);
+    // how it works when template is wrong:
+
+    int e = atoi("-1k23");
+    long f = atol("123sfas124125");
+    long long g = atoll("1242151231241251365174512548125");
+    double h = atof("f0.2136");
+
+    printf("\na = %d\nb = %ld\nc = %lld\nd = %.2f\n", e, f, g, h);
 
     return 0;
 }
