5ecca89589343cf3fa0fa17f350f7d5869815f69
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-24 11:36:23 +0300

    str template correct way

--- example.c
+++ example.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main(void)
 {
     double width = 2.88, height = 3.45, depth = 0.5;
     char name[] = "Chair";
-    char info[100];
-    
+    const size_t size = strlen(name) + 100;
+    char* info = malloc(size);
     const char template[] = "(%.3s: %.2f x %.2f x %.2f)";
 
     sprintf(info, template, name, width, height, depth);
     puts(info);
+
+    free(info);
 
     return 0;
 }
