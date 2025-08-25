5a32dd7a4636bf5e8d0710720db2b8a5a96b5aef
Pavel Tarakanov <pavel191094@icloud.com>
2025-08-25 11:01:13 +0300

    function prototype

--- example.c
+++ example.c
@@ -1,25 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
+#include "func.h"
 
 int main(void)
 {
-    int a = atoi("-123");
-    long b = atol("123124125");
-    long long c = atoll("12421512312412515");
-    double d = atof("0.2136");
-
-    printf("\na = %d\nb = %ld\nc = %lld\nd = %.2f\n", a, b, c, d);
-
-    // how it works when template is wrong:
-
-    int e = atoi("-1k23");
-    long f = atol("123sfas124125");
-    long long g = atoll("1242151231241251365174512548125");
-    double h = atof("f0.2136");
-
-    printf("\na = %d\nb = %ld\nc = %lld\nd = %.2f\n", e, f, g, h);
-
+    printf("P = %.2f", per_sq(3, 4));
     return 0;
 }
